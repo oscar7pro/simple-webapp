9a330bd322945eedf7eedd7c3d0006552b44c6c5
oscar7pro <62612674+oscar7pro@users.noreply.github.com>
2021-02-13 02:31:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,15 @@
-import os
-from flask import Flask
-from flaskext.mysql import MySQL      # For newer versions of flask-mysql 
-# from flask.ext.mysql import MySQL   # For older versions of flask-mysql
-app = Flask(__name__)
+import mysql.connector
+from mysql.connector import errorcode
 
-mysql = MySQL()
-
-mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or  'localhost'
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'db_user'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Passw0rd'
-app.config['MYSQL_DATABASE_DB'] = 'employee_db'
-app.config['MYSQL_DATABASE_HOST'] = mysql_database_host
-mysql.init_app(app)
-
-conn = mysql.connect()
-
-cursor = conn.cursor()
-
-@app.route("/")
-def main():
-    return "Welcome!"
-
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
-
-@app.route('/read from database')
-def read():
-    cursor.execute("SELECT * FROM employees")
-    row = cursor.fetchone()
-    result = []
-    while row is not None:
-      result.append(row[0])
-      row = cursor.fetchone()
-
-    return ",".join(result)
-
-if __name__ == "__main__":
-    app.run()
+try:
+  cnx = mysql.connector.connect(user='scott',
+                                database='employ')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cnx.close()
